c31dfb60b926880c9343381540c109faf4f28914
Alexander1998007 <invincible.ghost@yandex.ru>
2019-05-15 12:41:50 +0300

    header component
    Зробив зміну кольору хедера на scroll

--- src/app/navigation/header/header.component.ts
+++ src/app/navigation/header/header.component.ts
@@ -1,17 +1,34 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
+import { HostListener } from '@angular/core';
 
+declare const window: any;
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent{
   @Output() public sidenavToggle = new EventEmitter();
-  constructor() { }
 
-  ngOnInit() {
-  }
+  bgcolor = 'transparent';
+
+  // використовується для sidebar
   public onToggleSidenav = () => {
     this.sidenavToggle.emit();
+  }
+
+  // використовується для зміни кольора хедера
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+
+    // tslint:disable-next-line:variable-name
+    const number = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+    if (number > 1) {
+      // console.log('You are 100px from the top to bottom');
+      return this.bgcolor = 'white';
+    }
+    if (number < 1) {
+      return this.bgcolor = 'transparent';
+    }
   }
 }
--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,7 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { HostListener } from '@angular/core';
-
-declare const window: any;
 
 @Component({
   selector: 'app-search',
@@ -12,16 +9,4 @@ declare const window: any;
 export class SearchComponent implements OnInit {
 
   ngOnInit() { }
-
-  @HostListener('window:scroll', [])
-  onWindowScroll() {
-
-    // tslint:disable-next-line:variable-name
-    const number = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
-    if (number > 100) {
-      console.log('You are 100px from the top to bottom');
-    } else if (number > 500) {
-      console.log('You are 500px from the top to bottom');
-    }
-  }
 }
